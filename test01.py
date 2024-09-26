bfee682c1f11950a3ad1d58620fc3ab20841c9cf
JiyeonGong <gamja_da@naver.com>
2024-09-26 09:52:59 +0900

    git exam

---
+++ test01.py
@@ -0,0 +1,9 @@
+#git hub 연습 파일
+
+def addNum(a, b):
+    sum = a + b
+    return sum
+num01 = 10
+num02 = 20
+
+print(addNum(num01, num02))
---
+++ test02.py
@@ -0,0 +1,10 @@
+# git hub 연습 파일
+
+def mulNum(a, b):
+    result = a * b
+    return result
+
+num01 = 10
+num02 = 20
+print(mulNum(num01,num02))
+
